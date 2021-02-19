dec17d601f97fe99ae9c7bb0c09622a62ff2e872
Bikash Mondal <bikash@cbnits.com>
2021-02-19 17:25:07 +0530

    Dropdown fixation

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { EditorState } from "prosemirror-state"
+import { EditorState, Plugin } from "prosemirror-state"
 import { EditorView } from "prosemirror-view"
 import { Schema, DOMParser, Fragment } from "prosemirror-model"
 import { schema } from "./Schema.js";
@@ -17,6 +17,8 @@ const mySchema = new Schema({
   marks: schema.spec.marks
 });
 
+// const pm = new ProseMirror({ doc: mySchema.parseDOM(new DOMParser().parseFromString(html, "text/html"))})
+
 const myPlugins = exampleSetup({ schema: mySchema, menuBar: false });
 
 myPlugins.push(
@@ -26,6 +28,8 @@ myPlugins.push(
     }
   )
 );
+
+
 
 
 myPlugins.push(keymap({
@@ -41,39 +45,61 @@ class AnimalDropdownView {
     this.outerView = view
     this.getPos = getPos
 
-
-
-
     let divDom = document.createElement("div")
     divDom.classList.add('animal-container');
     let animals = [{ "title": "Dog", "src": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/dog.png" }, { "title": "Cat", "src": "https://www.rspca.org.uk/webContent/staticImages/Infographics/CatHappy1.jpg" }, { "title": "Cow", "src": "https://open3dmodel.com/wp-content/uploads/2020/03/1584438450_Cow.jpg" }]
+    this.animals = animals;
 
-    divDom.innerHTML = '<div class="animal-list">' +
-      animals
-        .map(i => '<div class="animal-item" data-src="' + i.src + '">' + i.title + "</div>")
-        .join("") +
-      "</div>";
-
-    divDom.querySelectorAll(".animal-item").forEach(function (itemNode, index) {
-      if (index == 0) {
-        itemNode.classList.add('selected-animal')
-      }
-      itemNode.addEventListener("click", function () {
-        let animalData = animals.find(row => row.title == itemNode.innerHTML);
-        let { $from, $to } = view.state.selection;
-        let $position = view.state.tr.selection.$from
-        const text = $position.doc.textBetween($position.before(), $position.pos, "\n", "\0");
-       
-        view.dispatch(view.state.tr.replaceSelectionWith(mySchema.nodes.image.create(animalData)))
-
-        // view.focus();
-      });
-    });
     this.dom = divDom
-    this.dom.focus();
-    this.innerView = null
+
+    this.update(node, null)
   }
 
+
+  update(view, lastState) {
+    let state = view.state
+    let { from, to } = state.selection
+    const $position = state.selection.$from;
+    let text = $position.doc.textBetween($position.before(), $position.pos, "\n", "\0");
+    let newText = text.replace(/[^\w\s]/gi, '')
+    let foundAnimals = this.animals.filter(animalRow => newText.replace(" ", "").trim() && animalRow.title.toLowerCase().indexOf(newText.replace(" ", "").trim().toLowerCase()) == 0);
+    
+    let textStart = text.indexOf(newText);
+    let start = view.coordsAtPos(textStart == 0 ? textStart : from - newText.length), end = view.coordsAtPos(to)
+
+    
+
+    if (foundAnimals.length > 0) {
+      this.dom.innerHTML = '<div class="animal-list">' +
+        foundAnimals
+          .map(i => '<div class="animal-item" data-src="' + i.src + '">' + i.title + "</div>")
+          .join("") +
+        "</div>";
+
+      this.dom.querySelectorAll(".animal-item").forEach(function (itemNode, index) {
+        if (index == 0) {
+          itemNode.classList.add('selected-animal')
+        }
+        itemNode.addEventListener("click", function () {
+          let animalData = foundAnimals.find(row => row.title == itemNode.innerHTML);
+          let { $from, $to } = view.state.selection;
+          let $position = view.state.tr.selection.$from
+
+          view.dispatch(view.state.tr.replaceSelectionWith(mySchema.nodes.image.create(animalData)))
+          view.dispatch(view.state.tr.insertText("", textStart == 0 ? textStart : from - newText.length, $position.pos));
+          view.focus();
+        });
+      });
+      view.dom.parentNode.appendChild(this.dom)
+      let box = this.dom.offsetParent.getBoundingClientRect()
+      let left = Math.max(start.left, start.left + 3)
+      this.dom.style.left = (left - box.left) + "px"
+      this.dom.style.top = (start.top + 35) + "px"
+    } else {
+      this.dom.innerHTML = ""
+    }
+
+  }
 
   stopEvent(event) {
     return this.innerView && this.innerView.dom.contains(event.target)
@@ -99,6 +125,11 @@ class Editor extends Component {
     };
   }
   componentDidMount() {
+
+    let animalDropdownPlugin = new Plugin({
+      view(editorView) { return new AnimalDropdownView(editorView) },
+    })
+    myPlugins.push(animalDropdownPlugin)
     let view = new EditorView(this.editor, {
       state: EditorState.create({
         doc: DOMParser.fromSchema(mySchema).parse(this.content),
@@ -109,18 +140,14 @@ class Editor extends Component {
         view.updateState(newState)
       },
       nodeViews: {
-        image(node, nodeView, getPos) { console.log(getPos); return new ImageView(node) },
+        image(node, nodeView, getPos) { return new ImageView(node) },
         animaldropdown(node, view, getPos) { return new AnimalDropdownView(node, view, getPos) },
 
       },
       handlePaste: (view, event, slice) => { console.log(event) },
       handleKeyDown: (view, e, pos) => {
-        console.log('keydown', pos)
         let { empty, $from, $to } = view.state.selection, content = Fragment.empty
-        // console.log($from, $to)
-        // if (!empty && $from.sameParent($to) && $from.parent.inlineContent)
-        content = $from.parent.content.content && $from.parent.content.content.find(rowContent => rowContent.type.name == "animaldropdown")
-
+        
         if (document.querySelector('.selected-animal')) {
           let active = document.querySelector('.selected-animal')
           let parentDiv = document.querySelector('.animal-list')
@@ -141,33 +168,26 @@ class Editor extends Component {
           }
 
           if (e.keyCode == 13) {
-            console.log(active.dataset.src);
             let animalData = {
               src: active.dataset.src,
               title: active.innerHTML
             }
+            let { from, to } = view.state.selection
+            const $position = view.state.selection.$from;
+            let text = $position.doc.textBetween($position.before(), $position.pos, "\n", "\0");
+            let newText = text.replace(/[^\w\s]/gi, '')
+            let textStart = text.indexOf(newText);
+
             view.dispatch(view.state.tr.replaceSelectionWith(mySchema.nodes.image.create(animalData)))
-            document.querySelector('.animal-container').remove();
-            // view.dispatch(mySchema.nodes.footnote.update());
             view.focus();
+            view.dispatch(view.state.tr.insertText("", textStart == 0 ? textStart : from - newText.length, $position.pos));
+
+           
+
           }
 
         }
       },
-      handleKeyPress: (view) => {
-
-        let { empty, $from, $to } = view.state.selection, content = Fragment.empty
-
-        content = $from.parent.content.content && $from.parent.content.content.find(rowContent => rowContent.type.name == "footnote")
-        console.log('content', $from, $to)
-
-        if (!document.querySelector('.animal-list')) {
-          view.dispatch(view.state.tr.insert(view.state.tr.doc.content.size, mySchema.nodes.animaldropdown.create()));
-          view.focus()
-        }
-
-
-      }
     });
 
     this.setState({ editorView: view })
